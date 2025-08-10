29d79831fb446e54668bf9a6b2e4438d4dc1d994
Muhammad Farel Andriani <muhammadfarelandriani@gmail.com>
2025-08-10 13:08:21 +0700

    update

--- home/script.js
+++
@@ -1,7 +0,0 @@
-function resizeIframe()
- { var iframe = document.getElementById('myIframe'); 
-    iframe.contentWindow.document.body.style.margin = '0'; 
-    iframe.contentWindow.document.body.style.padding = '0'; 
-    iframe.contentWindow.document.body.style.width = '100%';
-     iframe.contentWindow.document.body.style.height = '100%'; 
-} document.getElementById('myIframe').onload = resizeIframe;
